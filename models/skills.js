c3a78113bd179a5b212751744b6d2ada3d316f28
Jeannette130204 <jeannette130204@gmail.com>
2022-12-11 13:34:02 -0800

    Created project

---
+++ models/skills.js
@@ -0,0 +1,7 @@
+const mySkills = [
+    {skill: 'CSS', learned: true},
+    {skill: 'Html', learned: true},
+    {skill: 'JavaSript', learned: true},
+    {skill: 'jquery', learned: true},
+    {skill: 'Express', learned: false},
+]
